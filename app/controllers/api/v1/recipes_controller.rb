28d14750d4dd6cf8e83b6e5650bab6a2fe4de935
Noah Kochanowicz <noah.kochanowicz@agapered.com>
2016-05-07 23:24:36 -0400

    Changed to food description instead of buy guide

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -2,21 +2,21 @@ class Api::V1::RecipesController < Api::V1::BaseController
   def index
     response = {recipes: []} 
     offset = params[:page].to_i * params[:per_page].to_i
-    Bygdlnk.limit(params[:per_page].to_i).offset(offset).order(CN_Code: :desc).includes(:fdes, :ctgnme, :nutvals, :nutdes, :buygd, :wghts).each do |link|
+    Fdes.limit(params[:per_page].to_i).offset(offset).order(CN_Code: :desc).includes(:ctgnme, :nutvals, :nutdes, :buygd, :wghts).each do |food|
 
-      link.wghts.each do |weight|
+      food.wghts.each do |weight|
         response_hash = {
-          id: "#{link.CN_Code}-#{(weight.Gram_Weight*100).to_i}",
-          name: link.fdes.Abbr_Desc.titleize,
-          description: link.fdes.Descriptor, 
+          id: "#{food.CN_Code}-#{(weight.Gram_Weight*100).to_i}",
+          name: food.Abbr_Desc.titleize,
+          description: food.Descriptor, 
           weight: weight.Gram_Weight,
           amount: weight.Amount,
           measure_desc: weight.Measure_desc,
-          cnd_code: link.CN_Code,
-          category: link.fdes.ctgnme.Category_Description
+          cnd_code: food.CN_Code,
+          category: food.ctgnme.Category_Description
         }
 
-        link.nutvals.each do |nutval|
+        food.nutvals.each do |nutval|
           if !nutval.Nutrient_Value.nil? 
             value = (nutval.Nutrient_Value * (weight.Gram_Weight/100))
           else
--- app/models/fdes.rb
+++ app/models/fdes.rb
@@ -1,8 +1,10 @@
 class Fdes < ActiveRecord::Base
   self.table_name = "fdes"
 
+  has_many :wghts, foreign_key: "CN_Code", primary_key: "CN_Code"
+  has_many :nutvals, foreign_key: "CN_Code", primary_key: "CN_Code"
+
   belongs_to :bygdlnk, foreign_key: "CN_Code", primary_key: "CN_Code"
   has_one :buygd, through: :bygdlnk
-
   belongs_to :ctgnme, foreign_key: "Food_Category_Code", primary_key: "Food_Category_Code"
 end
