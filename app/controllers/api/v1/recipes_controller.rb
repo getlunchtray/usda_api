c70ff91032611f67eed6f097b2a0cb375087b059
Noah Kochanowicz <noah.kochanowicz@agapered.com>
2016-05-07 23:40:26 -0400

    Fixes

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -2,33 +2,38 @@ class Api::V1::RecipesController < Api::V1::BaseController
   def index
     response = {recipes: []} 
     offset = params[:page].to_i * params[:per_page].to_i
-    Fdes.limit(params[:per_page].to_i).offset(offset).order(CN_Code: :desc).includes(:ctgnme, :nutvals, :nutdes, :buygd, :wghts).each do |food|
+    if offset > Fdes.count
+      response[:end_of_results] = true 
+      expose response
+    else
+      Fdes.limit(params[:per_page].to_i).offset(offset).order(CN_Code: :desc).includes(:ctgnme, :nutvals, :nutdes, :wghts).each do |food|
 
-      food.wghts.each do |weight|
-        response_hash = {
-          id: "#{food.CN_Code}-#{(weight.Gram_Weight*100).to_i}",
-          name: food.Abbr_Desc.titleize,
-          description: food.Descriptor, 
-          weight: weight.Gram_Weight,
-          amount: weight.Amount,
-          measure_desc: weight.Measure_desc,
-          cnd_code: food.CN_Code,
-          category: food.ctgnme.Category_Description
-        }
+        food.wghts.each do |weight|
+          response_hash = {
+            id: "#{food.CN_Code}-#{(weight.Gram_Weight*100).to_i}",
+            name: food.Abbr_Desc.titleize,
+            description: food.Descriptor, 
+            weight: weight.Gram_Weight,
+            amount: weight.Amount,
+            measure_desc: weight.Measure_desc,
+            cnd_code: food.CN_Code,
+            category: food.ctgnme.Category_Description
+          }
 
-        food.nutvals.each do |nutval|
-          if !nutval.Nutrient_Value.nil? 
-            value = (nutval.Nutrient_Value * (weight.Gram_Weight/100))
-          else
-            value = nil
+          food.nutvals.each do |nutval|
+            if !nutval.Nutrient_Value.nil? 
+              value = (nutval.Nutrient_Value * (weight.Gram_Weight/100))
+            else
+              value = nil
+            end
+            response_hash[nutval.nutdes.Nutrient_Description] = value
           end
-          response_hash[nutval.nutdes.Nutrient_Description] = value
-        end
 
-        response[:recipes] << response_hash
+          response[:recipes] << response_hash
+        end
       end
+      
+      expose response
     end
-    
-    expose response
   end
 end
--- app/models/fdes.rb
+++ app/models/fdes.rb
@@ -4,6 +4,8 @@ class Fdes < ActiveRecord::Base
   has_many :wghts, foreign_key: "CN_Code", primary_key: "CN_Code"
   has_many :nutvals, foreign_key: "CN_Code", primary_key: "CN_Code"
 
+  has_many :nutdes, through: :nutvals
+
   belongs_to :bygdlnk, foreign_key: "CN_Code", primary_key: "CN_Code"
   has_one :buygd, through: :bygdlnk
   belongs_to :ctgnme, foreign_key: "Food_Category_Code", primary_key: "Food_Category_Code"
