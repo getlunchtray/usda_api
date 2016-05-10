1564bd33dc81649d58691182c904b69c7d25b640
Noah Kochanowicz <noah.kochanowicz@agapered.com>
2016-05-10 14:20:32 -0400

    Switches the query back to buygd

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -6,21 +6,21 @@ class Api::V1::RecipesController < Api::V1::BaseController
       response[:end_of_results] = true 
       expose response
     else
-      Fdes.limit(params[:per_page].to_i).offset(offset).order(CN_Code: :desc).includes(:ctgnme, :nutvals, :nutdes, :wghts).each do |food|
+      Bygdlnk.limit(params[:per_page].to_i).offset(offset).order(CN_Code: :desc).includes(:fdes, :buygd, :ctgnme, :nutvals, :nutdes, :wghts).each do |guide|
 
-        food.wghts.each do |weight|
+        guide.wghts.each do |weight|
           response_hash = {
-            id: "#{food.CN_Code}-#{(weight.Gram_Weight*100).to_i}",
-            name: food.Abbr_Desc.titleize,
-            description: food.Descriptor, 
+            id: "#{guide.CN_Code}-#{(weight.Gram_Weight*100).to_i}",
+            name: guide.fdes.Abbr_Desc.titleize,
+            description: guide.fdes.Descriptor, 
             weight: weight.Gram_Weight,
             amount: weight.Amount,
             measure_desc: weight.Measure_desc,
-            cnd_code: food.CN_Code,
-            category: food.ctgnme.Category_Description
+            cnd_code: guide.CN_Code,
+            category: guide.ctgnme.Category_Description
           }
 
-          food.nutvals.each do |nutval|
+          guide.nutvals.each do |nutval|
             if !nutval.Nutrient_Value.nil? 
               value = (nutval.Nutrient_Value * (weight.Gram_Weight/100))
             else
